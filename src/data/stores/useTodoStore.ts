3b58db35dd3c878e65fcd969051d2b0c80b6fbfc
Alexander Vetrov <notescape262@gmail.com>
2023-04-11 22:32:28 +0200

    added devtools and persist in zustand

--- src/data/stores/useTodoStore.ts
+++ src/data/stores/useTodoStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
 
 interface Todo {
 	id: number
@@ -13,35 +14,44 @@ interface TodoStore {
 	deleteTodo: (id: number) => void
 }
 
-export const useTodoStore = create<TodoStore>((set, get) => ({
-	todos: [],
-	createTodo: (title: string) => {
-		const { todos } = get()
-		const newTask = {
-			id: Date.now() + (Math.random() * 100 + Math.random()),
-			title,
-			createdAt: Date.now(),
-		}
+export const useTodoStore = create<TodoStore>()(
+	devtools(
+		persist(
+			(set, get) => ({
+				todos: [],
+				createTodo: (title: string) => {
+					const { todos } = get()
+					const newTask = {
+						id: Date.now() + (Math.random() * 100 + Math.random()),
+						title,
+						createdAt: Date.now(),
+					}
 
-		set({
-			todos: [newTask].concat(todos),
-		})
-	},
-	updateTodo: (id: number, title: string) => {
-		const { todos } = get()
+					set({
+						todos: [newTask].concat(todos),
+					})
+				},
+				updateTodo: (id: number, title: string) => {
+					const { todos } = get()
 
-		set({
-			todos: todos.map((todo) => ({
-				...todo,
-				title: todo.id === id ? title : todo.title,
-			})),
-		})
-	},
-	deleteTodo: (id: number) => {
-		const { todos } = get()
+					set({
+						todos: todos.map((todo) => ({
+							...todo,
+							title: todo.id === id ? title : todo.title,
+						})),
+					})
+				},
+				deleteTodo: (id: number) => {
+					const { todos } = get()
 
-		set({
-			todos: todos.filter((todo) => todo.id !== id),
-		})
-	},
-}))
+					set({
+						todos: todos.filter((todo) => todo.id !== id),
+					})
+				},
+			}),
+			{
+				name: 'todos-storage',
+			}
+		)
+	)
+)
